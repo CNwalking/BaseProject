0a3d6c1ffe38e49032a39dac96456a3f7cac410e
huqi <huqi@zhuojianchina.com>
2020-04-14 23:11:45 +0800

    druid and fastjson config

--- src/main/java/com/walking/project/common/WebConfig.java
+++ src/main/java/com/walking/project/common/WebConfig.java
@@ -1,7 +1,16 @@
 package com.walking.project.common;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.serializer.SerializerFeature;
+import com.alibaba.fastjson.support.config.FastJsonConfig;
+import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
+import org.springframework.boot.autoconfigure.web.HttpMessageConverters;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.*;
+
+import java.nio.charset.Charset;
+import java.util.List;
 
 /**
  * @Author: CNwalking
@@ -19,6 +28,38 @@ public class WebConfig extends WebMvcConfigurationSupport {
         registry.addResourceHandler("/webjars/**").addResourceLocations(
                 "classpath:/META-INF/resources/webjars/");
         super.addResourceHandlers(registry);
+    }
+
+    @Override
+    protected void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        FastJsonHttpMessageConverter fastConverter = new FastJsonHttpMessageConverter();
+        FastJsonConfig fastJsonConfig = new FastJsonConfig();
+        // 根据需要修改下面的配置,一般来说String为null则输出"",别的值为null就不显示
+        SerializerFeature[] serializerFeatures = new SerializerFeature[]{
+                //    输出key是包含双引号
+//                SerializerFeature.QuoteFieldNames,
+                //    是否输出为null的字段,若为null则显示该字段(注释掉表示:不显示null值)
+//                SerializerFeature.WriteMapNullValue,
+                //    数值字段如果为null，则输出为0
+//                SerializerFeature.WriteNullNumberAsZero,
+                //     List字段如果为null,输出为[],而非null
+                SerializerFeature.WriteNullListAsEmpty,
+                //    字符类型字段如果为null,输出为"",而非null
+                SerializerFeature.WriteNullStringAsEmpty,
+                //    Boolean字段如果为null,输出为false,而非null
+//                SerializerFeature.WriteNullBooleanAsFalse,
+                //    Date的日期转换器,默认yyyy-MM-dd HH:mm:ss
+                SerializerFeature.WriteDateUseDateFormat,
+                //    循环引用
+                SerializerFeature.DisableCircularReferenceDetect,
+        };
+
+        fastJsonConfig.setSerializerFeatures(serializerFeatures);
+        fastJsonConfig.setCharset(Charset.forName("UTF-8"));
+        fastConverter.setFastJsonConfig(fastJsonConfig);
+
+        HttpMessageConverter<?> converter = fastConverter;
+        converters.add(converter);
     }
 }
 
